de5b4b3bf5b5a7f124a5e6974582719927ab93c1
Manderson55 <mariela.z.anderson@gmail.com>
2017-06-19 15:54:05 -0600

    finished main logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,32 +13,76 @@
 
     var trainDatabase = firebase.database();
 
-    var trainName = "";
-    var destination = "";
+    var trainName = " ";
+    var destination = " ";
     var frequency = 0;
-    var nextArrival = "";
+    var nextArrival = " ";
     var minutesAway = 0;
+    var hoursAway = 0;
+    var firstTrainTime = "00:00" //military time
+    var inputError = false;
+    var timeOfDay = "";
+    // Functions
+    //this is not working!!!!!!!
+    function checkForInput(trainName, destination,firstTrainTime, frequency) {
+        if (trainName === null){
+            alert("You need to enter name for the train");
+            inputError = true;
+        } else 
+        if (destination === null){
+            alert("You need to enter a destination for the train");
+            inputError = true;
+        } else
+        if (firstTrainTime === "00:00") {
+            alert("You need to enter a valid time for the first train");
+            inputError = true;
+        } else
+        if (frequency === 0){
+            alert("You need to enter a frequency for the train");
+            inputError = true;
+        }
+ 
+    } //end checkforinput
 
     $("#addTrain").on("click", function(event) {
- 
-        event.preventDefault(); 
-        console.log('inside the onclick'); 
-        trainName = $("#trainName").val().trim();
-        console.log (trainName);
-        destination = $("#destination").val().trim();
-        console.log (destination);
-        firstTrainTime = $("#firstTrainTime").val();
-        console.log (firstTrainTime);
-        frequency = $("#frequency").val();
-        console.log (frequency);       
 
-        //upload the new train to the database
-        trainDatabase.ref().push({
-          trainName: trainName,
-          destination: destination,
-          firstTrainTime: firstTrainTime,
-          frequency: frequency
-        });
+    //    checkForInput();
+    //    console.log("Input Error = " + inputError);
+        //check to see if user entered all input fields
+    //    if (!inputError) {
+ 
+          event.preventDefault(); 
+          console.log('inside the onclick'); 
+          trainName = $("#trainName").val().trim();
+          console.log (trainName);
+          destination = $("#destination").val().trim();
+          console.log (destination);
+          firstTrainTime = $("#firstTrainTime").val();
+          console.log (firstTrainTime);
+          frequency = $("#frequency").val();
+          console.log (frequency);       
+
+          //upload the new train to the database
+          trainDatabase.ref().push({
+            trainName: trainName,
+            destination: destination,
+            firstTrainTime: firstTrainTime,
+            frequency: frequency
+          });
+
+          alert("Train Route Added");
+
+          // Clear all of the input boxes after train is added
+          $("#trainName").val("");
+          $("#destination").val("");
+          $("#firstTrainTime").val("00:00");
+          $("#frequency").val("0");
+
+    //   } // end if user entered all data
+    //     else {
+     //         console.log("Input Error = " + inputError);
+     //         alert ("Make sure you enter all the fields in the correct format!")
+     //     } //end else
   }); //end add train on click
 
   trainDatabase.ref().on("child_added", function(childSnapshot, prevChildKey) {
@@ -57,25 +101,60 @@
     console.log(firstTrainTime);
     console.log(frequency);
 
-    // convert to unix for calculations
-    //var empStartPretty = moment.unix(empStart).format("MM/DD/YY");
-
+    //need to convert the time to 1 year in the past since we can't make calculations
+    //in the future and we are not displaying the year
+    var firstTrainTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+    console.log( "converted first train time " + firstTrainTimeConverted);
+    console.log("converted first train time: " + firstTrainTimeConverted.format("hh:mm"));
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime));
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    
     // Calculate the next arrival time
-    //var empMonths = moment().diff(more than 59 min display in hours
+    //if more than 59 min display in hours and min
+  
+    var diffInTimes = moment().diff(moment(firstTrainTimeConverted), "minutes");
+    console.log ("difference between current time and time when first train started = " +
+                  diffInTimes);
 
-    // Calculate how many minutes away the next train is
-    //if it is more than 59 min display in hours
-    //var empBilled = empMonths * empRate;
-    //console.log(empBilled);
+    var timeApart = diffInTimes % frequency;
+    console.log ("frequency " + frequency);
+    console.log ("time apart " + timeApart);
+
+    minutesAway = frequency - timeApart;
+    console.log ("minutes away " + minutesAway);
+    if (minutesAway > 59){
+       hoursAway = Math.round(minutesAway / 60);
+       console.log(hoursAway);
+       hoursMinutesAway = minutesAway % 60;
+       console.log(hoursMinutesAway);
+       minutesAway = hoursAway + " hours " + hoursMinutesAway + " minutes";
+       console.log ("minutes away " + minutesAway);
+    }
+ 
+    var nextArrivalTime = moment().add(minutesAway, "minutes");
+    nextArrival = moment(nextArrivalTime).format("hh:mm");
+    console.log("NEXT ARRIVAL TIME: " + moment(nextArrivalTime).format("hh:mm"));
+
+    /* not working
+    if (nextArrival >= "12:00"){
+        timeOfDay = "PM"
+    } else {
+        timeOfDay = "AM"
+      }
+
+    console.log(timeOfDay);
+    */
+ 
 
     // display data in the window 
     $("#trainSchedule > tbody").append("<tr><td>" + trainName +
                                        "</td><td>" + destination +
                                        "</td><td>" + frequency + 
-                                       "</td><td>" + nextArrival + 
+                                       "</td><td>" + nextArrival + " " + timeOfDay +
                                        "</td><td>" + minutesAway + 
                                        "</td></tr>");
-});
+}); // end snapshot
 
 
 
