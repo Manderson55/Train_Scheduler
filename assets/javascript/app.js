8048e6b385a7b32dca9502b45b01efdec30ecac8
Manderson55 <mariela.z.anderson@gmail.com>
2017-06-20 13:39:57 -0600

    added jquery validation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,36 +20,14 @@
     var minutesAway = 0;
     var hoursAway = 0;
     var firstTrainTime = "00:00" //military time
-    var inputError = false;
+    var inputError = false; //if there was a problem with the input
     var timeOfDay = "";
-    // Functions
-    //this is not working!!!!!!!
-    function checkForInput(trainName, destination,firstTrainTime, frequency) {
-        if (trainName === null){
-            alert("You need to enter name for the train");
-            inputError = true;
-        } else 
-        if (destination === null){
-            alert("You need to enter a destination for the train");
-            inputError = true;
-        } else
-        if (firstTrainTime === "00:00") {
-            alert("You need to enter a valid time for the first train");
-            inputError = true;
-        } else
-        if (frequency === 0){
-            alert("You need to enter a frequency for the train");
-            inputError = true;
-        }
- 
-    } //end checkforinput
+
 
     $("#addTrain").on("click", function(event) {
 
-    //    checkForInput();
-    //    console.log("Input Error = " + inputError);
-        //check to see if user entered all input fields
-    //    if (!inputError) {
+    //check to see if user entered all input fields
+    //and they are correct
  
           event.preventDefault(); 
           console.log('inside the onclick'); 
@@ -78,11 +56,7 @@
           $("#firstTrainTime").val("00:00");
           $("#frequency").val("0");
 
-    //   } // end if user entered all data
-    //     else {
-     //         console.log("Input Error = " + inputError);
-     //         alert ("Make sure you enter all the fields in the correct format!")
-     //     } //end else
+  
   }); //end add train on click
 
   trainDatabase.ref().on("child_added", function(childSnapshot, prevChildKey) {
@@ -111,7 +85,6 @@
     console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
     
     // Calculate the next arrival time
-    //if more than 59 min display in hours and min
   
     var diffInTimes = moment().diff(moment(firstTrainTimeConverted), "minutes");
     console.log ("difference between current time and time when first train started = " +
@@ -123,6 +96,13 @@
 
     minutesAway = frequency - timeApart;
     console.log ("minutes away " + minutesAway);
+
+ 
+    var nextArrivalTime = moment().add(minutesAway, "minutes");
+    nextArrival = moment(nextArrivalTime).format("hh:mm A");
+    console.log("NEXT ARRIVAL TIME: " + moment(nextArrivalTime).format("hh:mm A"));
+
+
     if (minutesAway > 59){
        hoursAway = Math.round(minutesAway / 60);
        console.log(hoursAway);
@@ -130,22 +110,9 @@
        console.log(hoursMinutesAway);
        minutesAway = hoursAway + " hours " + hoursMinutesAway + " minutes";
        console.log ("minutes away " + minutesAway);
-    }
- 
-    var nextArrivalTime = moment().add(minutesAway, "minutes");
-    nextArrival = moment(nextArrivalTime).format("hh:mm");
-    console.log("NEXT ARRIVAL TIME: " + moment(nextArrivalTime).format("hh:mm"));
-
-    /* not working
-    if (nextArrival >= "12:00"){
-        timeOfDay = "PM"
     } else {
-        timeOfDay = "AM"
-      }
-
-    console.log(timeOfDay);
-    */
- 
+       minutesAway = minutesAway + " minutes";
+    }
 
     // display data in the window 
     $("#trainSchedule > tbody").append("<tr><td>" + trainName +
